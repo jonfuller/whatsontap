66f1470cc6efa517e43a95677cdbccab97face90
Jon Fuller <jcfuller@sep.com>
2012-11-02 16:14:29 -0400

    initial commit

---
+++ whatsontap.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+require 'mechanize'
+
+get '/' do
+  "<h1>Sunking Beers</h1>" + get_sunking_beers.map{|beer| "<li>#{beer}</li>"}.join
+end
+
+
+def get_sunking_beers
+  url = "http://www.sunkingbrewing.com/index.php"
+  
+  agent = Mechanize.new
+  age_page = agent.get(url)
+  main_page = age_page.forms.first.submit(age_page.forms.first.submits[1])
+  ontap = main_page.search(".ontap")
+  
+  raw = ontap.text
+  
+  beers = ontap.text.split("\n").map{|s| s.strip}.select{|s| s.length > 0}.drop(2).map{|s| s.gsub('Seasonal Beer', '')}
+end
